e4ea40bf94356318ca09551db9b356e4cb96dc07
parkmyengbae <gambler1541@gmail.com>
2022-09-25 10:41:10 +0900

    ES6 형식으로 변경

--- src/App.vue
+++ src/App.vue
@@ -2,85 +2,86 @@
   <div>
     <todo-header></todo-header>
     <todo-input v-on:addTodoItem="addOneItem"></todo-input>
-    <todo-list 
-    v-bind:propsdata="todoItems" 
-    v-on:removeItem="removeOneItem" 
-    v-on:toggleItem="toggleOneItem"></todo-list>
-    <todo-footer
-    v-on:clearAll="clearAllItem"></todo-footer>
+    <todo-list
+      v-bind:propsdata="todoItems"
+      v-on:removeItem="removeOneItem"
+      v-on:toggleItem="toggleOneItem"
+    ></todo-list>
+    <todo-footer v-on:clearAll="clearAllItem"></todo-footer>
   </div>
 </template>
 
 <script>
-import TodoHeader from './components/TodoHeader.vue'
-import TodoInput from './components/TodoInput.vue'
-import TodoList from './components/TodoList.vue'
-import TodoFooter from './components/TodoFooter.vue'
+import TodoHeader from "./components/TodoHeader.vue";
+import TodoInput from "./components/TodoInput.vue";
+import TodoList from "./components/TodoList.vue";
+import TodoFooter from "./components/TodoFooter.vue";
 
 export default {
-  data: function(){
+  data: function () {
     return {
       todoItems: [],
-    }
+    };
   },
-  created: function(){
-    if(localStorage.length > 0){
-        for(let i = 0;i < localStorage.length; i++){
-            if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){
-                this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
-            }
+  created: function () {
+    if (localStorage.length > 0) {
+      for (let i = 0; i < localStorage.length; i++) {
+        if (localStorage.key(i) !== "loglevel:webpack-dev-server") {
+          this.todoItems.push(
+            JSON.parse(localStorage.getItem(localStorage.key(i)))
+          );
         }
+      }
     }
   },
-  methods:{
-    addOneItem: function(todoItem){
-      var obj = {complated: false, item: todoItem};
+  methods: {
+    addOneItem: function (todoItem) {
+      const obj = { complated: false, item: todoItem };
       localStorage.setItem(todoItem, JSON.stringify(obj));
       this.todoItems.push(obj);
     },
 
-    removeOneItem: function(todoItem, index){
+    removeOneItem: function (todoItem, index) {
       localStorage.removeItem(todoItem.item);
       this.todoItems.splice(index, 1);
     },
 
-    toggleOneItem: function(todoItem, index){
+    toggleOneItem: function (todoItem, index) {
       // todoItem.complated = !todoItem.complated;
-      this.todoItems[index].complated = !this.todoItems[index].complated 
+      this.todoItems[index].complated = !this.todoItems[index].complated;
       localStorage.removeItem(todoItem.item);
-      localStorage.setItem(todoItem.item, JSON.stringify(todoItem)); 
+      localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
     },
-    clearAllItem: function(){
+    clearAllItem: function () {
       localStorage.clear();
       this.todoItems = [];
-    }
-    
+    },
   },
-  components:{
+  components: {
     TodoHeader,
     TodoInput,
     TodoList,
-    TodoFooter
-  }
-}
+    TodoFooter,
+  },
+};
 </script>
 
 <style>
-  body {
-    text-align: center;
-    background-color: #F6F6F6;
-  }
+body {
+  text-align: center;
+  background-color: #f6f6f6;
+}
 
-  input {
-    border-style: groove;
-    width: 200px;
-  }
+input {
+  border-style: groove;
+  width: 200px;
+}
 
-  button {
-    border-style: groove;
-  }
+button {
+  border-style: groove;
+}
 
-  .shadow{
-    box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
-  }
+.shadow {
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+}
 </style>
